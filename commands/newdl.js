0a746d4f209f5161d4bc669687260299ec8f06f0
justintroy <65443707+justintroy@users.noreply.github.com>
2021-01-24 16:09:33 +0800

    refactor: date parsing and remove unnecessary code.

--- commands/newdl.js
+++ commands/newdl.js
@@ -5,13 +5,11 @@ const { Events } = require("../dbObjects");
 
 const usageMessage = "Invalid args.\n```Usage: !newdl <date: MM/DD/YYYY> <time: HH:00> <AM/PM> <name>\nEx: !newdl 08/07/2021 7:00 PM Birthday```";
 const dateFormat = [
-    "MM/DD/YYYY h:mmA",
-    "MM/DD h:mmA",
     "MM/DD/YYYY h:mm A",
     "MM/DD h:mm A",
-    "MM/DD/YY h:mmA",
     "MM/DD/YY h:mm A",
 ];
+const dateTimeSQLFormat = "YYYY-MM-DDTHH:mm:ss.SSSZ";
 
 function extractEventName(arguments) {
     const name = arguments.replace(/\d{1,2}[\-|\.|\/]\d{1,2}[\-|\.|\/]\d{2,4}/g, "");
@@ -38,12 +36,11 @@ module.exports = {
         const argsStr = args.join(" ");
 
         if (!moment(argsStr, dateFormat).isValid()) return message.reply(usageMessage);
+        if (!isValidDate(argsStr)) return message.reply(usageMessage);
 
-        console.log(isValidDate(argsStr));
-        if(!isValidDate(argsStr)) return message.reply(usageMessage);
-
-        const eventDate = moment(argsStr, dateFormat).format("YYYY-MM-DDTHH:mm:ss.SSSZ");
-        const eventTime = moment(argsStr, dateFormat).format("H mm");
+        const eventDate = moment(argsStr, dateFormat).format(dateTimeSQLFormat);
+        const eventDateReadable = moment(eventDate, dateTimeSQLFormat).format("MMMM DD, YYYY");
+        const eventTime = moment(argsStr, dateFormat).format("h:mmA");
         const eventName = extractEventName(argsStr);
 
         try {
@@ -56,7 +53,12 @@ module.exports = {
                 .setColor('#0099ff')
                 .setTitle(eventName)
                 .setAuthor(message.author.username, message.author.displayAvatarURL())
-                .setDescription(`Date: ${eventDate}\nTime: ${eventTime}\n\n'${event.name}' added.`);
+                .setDescription(`
+                    Date: ${eventDateReadable}
+                    Time: ${eventTime}
+
+                    '${event.name}' added.
+                `);
 
             return message.reply(eventDetails);
 
