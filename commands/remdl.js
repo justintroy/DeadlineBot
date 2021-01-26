2c1cfaec6247f228cec30fd1b4b5c3e19c808b5d
justintroy <65443707+justintroy@users.noreply.github.com>
2021-01-26 10:43:04 +0800

    feat: remdl command, removes a deadline.

---
+++ commands/remdl.js
@@ -0,0 +1,17 @@
+const { Events } = require("../dbObjects");
+
+module.exports = {
+    name: 'remdl',
+    description: 'Removes a deadline',
+    args: true,
+    usage: '<deadline-name>',
+    guildOnly: true,
+    async execute(message, args) {
+        const deadlineName = args.join(" ");
+
+        const rowCount = await Events.destroy({ where: { name: deadlineName } });
+        if (!rowCount) return message.reply(`\`'${deadlineName}' is not present in database. !deadlines to check all available names. Names are case-sensitive.\``);
+
+        return message.reply(`\`'${deadlineName}' is deleted.\``);
+    },
+};
